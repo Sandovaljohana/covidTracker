88dc045744a10f8f4d42509b171c49934ed0cb9a
sandovaljohana <sandovaljohana0207@gmail.com>
2023-12-19 18:00:48 +0100

    add new folders, config, atoms, molecules, router

---
+++ src/components/configuration/Router.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import  from '..//'
+import  from '..//'
+
+
+const Router = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={< />} path='' />
+                    <Route element={< />} path='' />
+                </Routes>
+            </BrowserRouter>
+        </>
+
+    )
+}
+
+export default Router
