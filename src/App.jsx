6ee9966c7f0a1ef99eb56c0d2b53eca34be56ccc
sandovaljohana <sandovaljohana0207@gmail.com>
2024-01-08 12:35:57 +0100

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,4 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-import Router from "./components/configuration/Router";
-
-
-const App = () => {
-
-  return (
-    <>
-      <Router />
-=======
 import './App.css'
 import NavButton from './components/buttons/NavButton'
 
@@ -17,7 +7,6 @@ function App() {
   return (
     <>
  <NavButton />
->>>>>>> dd503ddc0d93a5bd09a5c5c3aac1403de209042d
     </>
   )
 }
